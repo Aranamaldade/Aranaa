acfab7171bbd98966dc23e02e5ebec0795f96f37
Pablo Diaz <129523254+Aranamaldade@users.noreply.github.com>
2024-09-04 17:29:04 -0300

    alteraçoes

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ avanca.forEach(button => {
         const atual = document.querySelector('.ativo')
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
         
-        atual.classList
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+            
+   
     } )
 })   
